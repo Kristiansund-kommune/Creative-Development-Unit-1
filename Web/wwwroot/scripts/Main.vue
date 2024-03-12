d8630c35ea26d53782c25029cbb8f79195d65727
NicoNeubauer <niconeubauer.me@gmail.com>
2024-03-12 17:59:16 +0100

    confetty and modal changes

--- Web/wwwroot/scripts/Main.vue
+++ Web/wwwroot/scripts/Main.vue
@@ -5,6 +5,8 @@
 			<img class="logo" src="../styles/logo.svg" alt="logo">
 		</div>
 		<button @click="showModal = true">Show Modal</button>
+		<button @click="launchConfetti">Celebrate!</button>
+		<div v-if="showMessage" class="level-up-message">Level Up!</div>
 		<div v-if="showModal" class="modal">
 			<div class="modal-content">				
 				<div>
@@ -38,6 +40,19 @@
   width: auto;
 }
 
+.level-up-message {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 3rem;
+  color: yellow;
+  z-index: 2; /* Ensure it's visible above other elements */
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 20px;
+  border-radius: 10px;
+}
+
 .modal {
   display: block;
   position: fixed;
@@ -55,8 +70,9 @@
 	padding: 15px;
 	border: 1PX solid black;
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(60px, 0.5fr)); /* Creates a responsive grid where each cell has a min width of 120px */
-  gap: 10px; /* Adjusts the gap between grid items */
+  border-radius: 10px 10px 0px 0px;
+  grid-template-columns: repeat(auto-fill, minmax(80px, 0.5fr)); /* Creates a responsive grid where each cell has a min width of 120px */
+  gap: 20px; /* Adjusts the gap between grid items */
 }
 
 
@@ -84,28 +100,33 @@
 
 .selectable-div {
   text-align: center;
-  padding: 10px;
+  padding: 0px;
+  border-radius: 5px;
   cursor: pointer;
   color: white; /* White text */
   height: 60px;
   width: 60px;
+  font-size: 2em;
   display: flex;
   justify-content: center;
   align-items: center;
 }
 
 .red {
-  background-color: rgb(185, 17, 17);
+  background-color: rgb(165, 30, 30);
 }
 
 .green {
-  background-color: rgb(45, 128, 45);
+  background-color: rgb(18, 110, 18);
 }
 </style>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import Map from "@/scripts/Map.vue";
+import confetti from "canvas-confetti";
+
+const showMessage = ref(false);
 
 const showModal = ref(false);
 
@@ -116,20 +137,20 @@ interface Div {
 }
 
 const divs = ref<Div[]>([
-	{ id: 1, name: '<i class="fa-solid fa-bicycle"></i>', selected: false },
-	{ id: 2, name: '<i class="fa-solid fa-bicycle"></i>', selected: false },
-	{ id: 3, name: '<i class="fa-solid fa-bicycle"></i>', selected: true },
-	{ id: 4, name: '<i class="fa-solid fa-bicycle"></i>', selected: false },
-	{ id: 5, name: '<i class="fa-solid fa-bicycle"></i>', selected: true },
-	{ id: 6, name: '<i class="fa-solid fa-bicycle"></i>', selected: false },
-	{ id: 7, name: '<i class="fa-solid fa-bicycle"></i>', selected: true },
-	{ id: 8, name: '<i class="fa-solid fa-bicycle"></i>', selected: true },
-	{ id: 9, name: '<i class="fa-solid fa-bicycle"></i>', selected: false },
-	{ id: 0, name: '<i class="fa-solid fa-bicycle"></i>', selected: false },
-	{ id: 9, name: '<i class="fa-solid fa-bicycle"></i>', selected: false },
-	{ id: 42, name: '<i class="fa-solid fa-bicycle"></i>', selected: false },
-	{ id: 29, name: '<i class="fa-solid fa-bicycle"></i>', selected: false },
-	{ id: 20, name: '<i class="fa-solid fa-bicycle"></i>', selected: false },
+	{ id: 1, name: '<i class="fa-solid fa-square-parking"></i>', selected: false },
+	{ id: 2, name: '<i class="fa-solid fa-square-parking"></i>', selected: false },
+	{ id: 3, name: '<i class="fa-solid fa-square-parking"></i>', selected: true },
+	{ id: 4, name: '<i class="fa-solid fa-square-parking"></i>', selected: false },
+	{ id: 5, name: '<i class="fa-solid fa-square-parking"></i>', selected: true },
+	{ id: 6, name: '<i class="fa-solid fa-square-parking"></i>', selected: false },
+	{ id: 7, name: '<i class="fa-solid fa-square-parking"></i>', selected: true },
+	{ id: 8, name: '<i class="fa-solid fa-square-parking"></i>', selected: true },
+	{ id: 9, name: '<i class="fa-solid fa-square-parking"></i>', selected: false },
+	{ id: 0, name: '<i class="fa-solid fa-square-parking"></i>', selected: false },
+	{ id: 22, name: '<i class="fa-solid fa-square-parking"></i>', selected: false },
+	{ id: 42, name: '<i class="fa-solid fa-square-parking"></i>', selected: false },
+	{ id: 29, name: '<i class="fa-solid fa-square-parking"></i>', selected: false },
+	{ id: 20, name: '<i class="fa-solid fa-square-parking"></i>', selected: false },
 ]);
 
 const toggleSelection = (id: number) => {
@@ -137,5 +158,31 @@ const toggleSelection = (id: number) => {
 	if (div) {
 		div.selected = !div.selected;
 	}
+};
+
+const launchConfetti = () => {
+  // Show the "Level Up!" message
+  showMessage.value = true;
+
+  // Launch confetti immediately
+  confetti({
+    particleCount: 100,
+    spread: 70,
+    origin: { y: 0.5 },
+  });
+
+  // Hide the message after 1 second
+  setTimeout(() => {
+    showMessage.value = false;
+  }, 1000);
+
+  // Launch confetti again after a 0.5-second delay
+  setTimeout(() => {
+    confetti({
+      particleCount: 200,
+      spread: 220,
+      origin: { y: 0.5 },
+    });
+  }, 500);
 };
 </script>
