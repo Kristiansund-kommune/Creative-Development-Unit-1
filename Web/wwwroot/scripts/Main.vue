ddc7a37b198831f209742d5a0085de2aeb24eb89
NicoNeubauer <niconeubauer.me@gmail.com>
2024-03-12 14:22:13 +0100

    some frontend

--- Web/wwwroot/scripts/Main.vue
+++ Web/wwwroot/scripts/Main.vue
@@ -1,11 +1,27 @@
 <template>
 	<div style="height: 100vh; width: 100vw">
 		<Map></Map>	
+		<div class="logo-container">
+			<img class="logo" src="../styles/logo.svg" alt="logo">
+		</div>	
 	</div>
 </template>
 
 <style scoped>
-
+  .logo-container {
+	position: absolute;
+	top: 0;
+	left: 0;
+	display: flex;
+	align-items: right;
+	padding: 10px 25px;
+	background-color: rgb(240, 239, 239);
+  }
+  
+  .logo {
+	height: 50px;
+	width: auto;
+  }
 </style>
 
 <script setup lang="ts">
---
+++ Web/wwwroot/scripts/top.vue
@@ -0,0 +1,36 @@
+<template>
+	<nav class="navbar">
+		<div class="logo-container">
+			<img src="../styles/logo.svg" alt="SYkkel logo" class="logo">
+		</div>
+	</nav>
+</template>
+  
+  <style scoped>
+  .navbar {
+	width: 100%;
+	background-color: #f7f7f7;
+	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	top: 0;
+	left: 0;
+	z-index: 1000;
+	display: flex;
+	justify-content: left;
+	padding: 10px 0;
+	margin-bottom: 100px;
+  }
+  
+  .logo-container {
+	display: flex;
+	align-items: right;
+	padding: 10px 25px;
+  }
+  
+  .logo {
+	height: 75px;
+	width: auto;
+  }
+  </style>
+  
+  <script setup lang="ts">
+  </script>
