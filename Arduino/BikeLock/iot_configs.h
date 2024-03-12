dc974f50d559af821f52efce07f43a39155a5649
Erlend Dullaert <erlend.johan.grytnes.dullaert@kristiansund.kommune.no>
2024-03-12 15:29:09 +0100

    connected to iot hub

---
+++ Arduino/BikeLock/iot_configs.h
@@ -0,0 +1,8 @@
+#define IOT_CONFIG_WIFI_SSID "Kulturfabrikken-Gjest"
+#define IOT_CONFIG_WIFI_PASSWORD "Kfgjest!"
+
+#define IOT_CONFIG_IOTHUB_FQDN "creativeDevelopmentUnit.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "bikeLockPrototype"
+#define IOT_CONFIG_DEVICE_KEY "qK53PSOzH0xnDUkV8ozlnmkEnzlC8qaWVAIoTD2AmFc="
+
+#define TELEMETRY_FREQUENCY_MILLISECS 2000
